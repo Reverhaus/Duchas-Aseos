5881df016670219faf89d7987b946a31a3899e18
Javier Bailén <156017622+Reverhaus@users.noreply.github.com>
2026-03-07 03:29:48 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,37 +1,33 @@
-const CACHE_NAME = 'aseo-v1';
+const CACHE_NAME = 'aseo-pwa-v1';
+const BASE_PATH = '/Duchas-Aseos';
+
 const ASSETS = [
-  '/',
-  '/index.html',
-  '/manifest.json',
+  BASE_PATH + '/',
+  BASE_PATH + '/index.html',
+  BASE_PATH + '/manifest.json',
   'https://cdn.tailwindcss.com',
   'https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700;900&display=swap'
 ];
 
-// Instalar Service Worker
 self.addEventListener('install', (e) => {
   e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS);
-    })
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
 });
 
-// Activar Service Worker
 self.addEventListener('activate', (e) => {
   e.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(
-        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
-      );
-    })
+    caches.keys().then((keys) => 
+      Promise.all(
+        keys.filter((key) => key !== CACHE_NAME)
+            .map((key) => caches.delete(key))
+      )
+    )
   );
 });
 
-// Interceptar peticiones
 self.addEventListener('fetch', (e) => {
   e.respondWith(
-    caches.match(e.request).then((res) => {
-      return res || fetch(e.request);
-    })
+    caches.match(e.request).then((res) => res || fetch(e.request))
   );
 });
